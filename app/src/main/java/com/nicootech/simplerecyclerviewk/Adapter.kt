62a0478fb5b7fa1c38b788791ca7e5280a1c7ac1
Ethan Rahimian <nicootech@gmail.com>
2021-07-08 12:05:42 +0900

    Adapter & ViewHolder - Simple RecyclerView in Kotlin

---
+++ app/src/main/java/com/nicootech/simplerecyclerviewk/Adapter.kt
@@ -0,0 +1,50 @@
+package com.nicootech.simplerecyclerviewk
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class Adapter(private val list :List<Item>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imageView : ImageView = itemView.findViewById(R.id.image_view)
+        val textView1 : TextView = itemView.findViewById(R.id.text_1)
+        val textView2 : TextView = itemView.findViewById(R.id.text_2)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
+            parent,false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = list[position]
+
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+
+        if(position %3 == 0) {
+            holder.textView1.text = "Hip!"
+            holder.textView1.setBackgroundColor(Color.BLUE)
+        }
+        if(position %5 == 0){
+            holder.textView1.text = "Pop!"
+            holder.textView1.setBackgroundColor(Color.YELLOW)
+        }
+        if(position %15 ==0){
+            holder.textView1.text = "HipPop!"
+            holder.textView1.setBackgroundColor(Color.GREEN)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- app/src/main/java/com/nicootech/simplerecyclerviewk/MainActivity.kt
+++ app/src/main/java/com/nicootech/simplerecyclerviewk/MainActivity.kt
@@ -2,10 +2,39 @@ package com.nicootech.simplerecyclerviewk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = generateDummyList(100)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = Adapter(list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+
     }
+
+    private fun generateDummyList(size: Int): List<Item> {
+        val list = ArrayList<Item>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_account
+                1 -> R.drawable.ic_baseline
+                else -> R.drawable.ic_brightness
+            }
+            val item = Item(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
+    }
+
+
 }
